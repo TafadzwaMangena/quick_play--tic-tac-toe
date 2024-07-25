ad6375eb410cdf2c2d23c4dd2baed559a4dae406
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2024-07-25 09:03:17 +0000

    Add js variables

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const playersTurn = document.querySelector("#playersTurn");
+const cellBlocks = document.querySelectorAll(".cellBlock");
+const restartGameBtn = document.querySelector("#restartGameBtn");
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
+let gameState = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let gameActive = true;
+
+let Winmessage = () => `${currentPlayer} has won this round!`;
+let drawmessage = () => `This round ended in a draw`;
+let currentPlayerTurn = () => `${currentPlayer}'s turn to play`;
