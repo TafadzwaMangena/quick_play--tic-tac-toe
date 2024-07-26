91590502a7329e5cbf7a47d10f8d660808617d43
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2024-07-26 17:21:40 +0000

    Add comments to functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,9 +21,16 @@ let Winmessage = () => `${currentPlayer} has won this round!`;
 let drawmessage = () => `This round ended in a draw`;
 let currentPlayerTurn = () => `${currentPlayer}'s turn to play`;
 
+/**
+ * To initialize the game upon start.
+ */
 startGame();
 
-
+/**
+ * Fucntion to target each cellblock; added event listener to target the cellblocks when clicked,
+ * Add event listener to the restart game button when it is clicked,
+ * Upon start the game is set to be active and the current player's name is displayed.
+ */
 function startGame(){
     cellBlocks.forEach(cellBlock => cellBlock.addEventListener("click", cellBlockClicked));
     restartGameBtn.addEventListener("click", restartGame);
@@ -31,6 +38,11 @@ function startGame(){
     gameActive = true
 }
 
+/**
+ * Function to collect data from the data cell indexes to check if the cell blocks are not empty,
+ * If cells blocks are empty or the game not active, nothing happens.
+ * Otherwise collect cell block info and check winner.
+ */
 function cellBlockClicked(){
     const dataCellIndex = this.getAttribute("data-cell-index");
 
@@ -41,6 +53,10 @@ function cellBlockClicked(){
     checkWinner();
 }
 
+/**
+ * Updating the placeholders,
+ * replace the text content of the cellblocks to current player, either "X" or "O".
+ */
 function updateCellBlock(cellBlock, index){
     gameState[index] = currentPlayer;
     cellBlock.textContent = currentPlayer
